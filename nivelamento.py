e10cb6e20f270c35071a0eee68f5f540798bcde2
Mill <millsibeon@gmail.com>
2025-02-13 12:44:39 -0300

    Update nivelamento.py

--- nivelamento.py
+++ nivelamento.py
@@ -5,15 +5,13 @@ for i in range(15):
     genero = input(f"Pessoa nº {i+1}\nGênero (M - Masculino; F - Feminino): ").lower()
 
     while genero != "m" and genero != "f":
-        print("Resposta inválida. Digite 'M' se o seu gênero for masculino e 'F' se o seu gênero for feminino.")
-        genero = input("Gênero (M - Masculino; F - Feminino): ").lower()
+        genero = input("Resposta inválida. Digite 'M' se o seu gênero for masculino e 'F' se o seu gênero for feminino.\nGênero (M - Masculino; F - Feminino): ").lower()
 
     else:
         altura = int(input("Digite a sua altura, em cm, sem casas decimais: "))
         
         while altura <= 0:
-            print("Valor inválido. Tente novamente.")
-            altura = int(input("Digite a sua altura, em cm, sem casas decimais: "))
+            altura = int(input("Valor inválido. Tente novamente.\nDigite a sua altura, em cm, sem casas decimais: "))
 
         else:
             if genero == "m":
@@ -21,10 +19,9 @@ for i in range(15):
             elif genero == "f":
                 alt_f.append(altura)
 
-
 alt_todos = alt_m + alt_f
-print("Maior altura:",max(alt_todos))
-print("Menor altura:",min(alt_todos))
+print("Maior altura:", max(alt_todos))
+print("Menor altura:", min(alt_todos))
  
 print("Média de altura das pessoas do gênero masculino:", sum(alt_m)/len(alt_m))
 print("Número de pessoas do gênero feminino:", len(alt_f))
